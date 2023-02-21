f232d3357252534dd5703ee0dc9e4e2bd897e688
Guilherme Siqueira <siqueiraguilherme920@gmail.com>
2023-02-20 21:39:16 -0300

    Added body background

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const lamp = document.querySelector('#lamp')
 const btn_on = document.querySelector('#turnOn')
 const btn_off = document.querySelector('#turnOff')
+const body = document.querySelector('body')
 
 //Events
 btn_on.addEventListener('click', () => {
     lamp.src = './assets/ligada.jpg'
+    body.classList.add('on')
 })
 
 btn_off.addEventListener('click', () => {
     lamp.src = './assets/desligada.jpg'
+    body.classList.remove('on')
 })
